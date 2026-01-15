db51b70d4dc63aff0007430da98293d9a43e3cf4
Aman Kashyap <ak6572840@gmail.com>
2026-01-15 18:49:11 +0530

    remove unused code change

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,12 +14,12 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 app.use((req, res, next) => {
   console.log("Request Origin:", req.headers.origin);
@@ -33,7 +33,7 @@ const PORT = process.env.PORT;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../ChatApp/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../ChatApp/dist/index.html"));
   });
 }
