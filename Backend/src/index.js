1f7e080504224d13fb3dc256c2f9d891232885d7
Aman Kashyap <ak6572840@gmail.com>
2026-01-15 17:31:21 +0530

    packagejson change

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,7 +7,7 @@ import messageRoutes from "./routes/message.route.js";
 
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-import { path } from "path";
+import path from "path";
 dotenv.config();
 // const app = express();
 const __dirname = path.resolve();
